d43295a94ddb5c6dd824c23af14d5345cc9e6e03
MydarMo <hameedahmohammed7@yahoo.com>
2024-01-22 16:32:46 +0000

    fix api index error handling

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import { request } from './helpers';
+import { request } from "./helpers";
 
 /**
  * Pull vehicles information
@@ -8,26 +8,34 @@ import { request } from './helpers';
  */
 // TODO: All API related logic should be made inside this function.
 export default async function getData() {
-  // fetch the list of vehicles
-  const response = await fetch('/api/vehicles.json');
-  const data = await response.json();
-
-  // map through the list of vehicles to access the apiUrl for each vehicle to return the list of only successful APIs
-  const vehicleData = await Promise.allSettled(
-    data.map(async (res) => (await fetch(res.apiUrl)).json())
-  );
-
-  // merge both data and vehicleData to form a complete json object for each car, excluding unfilfilled response and vehicles without price
-  const vehicles = [];
-
-  for (let i = 0; i < data.length; i + 1) {
-    if (
-      vehicleData[i].status === 'fulfilled'
-            && vehicleData[i].value.price
-    ) {
-      vehicles.push({ ...data[i], ...vehicleData[i].value });
+    // fetch the list of vehicles
+    const response = await fetch("/api/vehicles.json");
+    if (!response.ok) {
+        // Handle HTTP error
+        throw new Error(`HTTP error! Status: ${response.status}`);
     }
-  }
+    const data = await response.json();
 
-  return vehicles;
+    // map through the list of vehicles to access the apiUrl for each vehicle to return the list of only successful APIs
+    const vehicleData = await Promise.allSettled(
+        data.map(async (res) => (await fetch(res.apiUrl)).json())
+    );
+
+    console.log({ vehicleData, data });
+
+    // merge both data and vehicleData to form a complete json object for each car, excluding unfilfilled response and vehicles without price
+    const vehicles = [];
+
+    for (let i = 0; i < data.length; i++) {
+        if (
+            vehicleData[i].status === "fulfilled" &&
+            vehicleData[i].value.price
+        ) {
+            vehicles.push({ ...data[i], ...vehicleData[i].value });
+        } else {
+            console.log(`HTTP error! Status: failed`);
+        }
+    }
+
+    return vehicles;
 }
