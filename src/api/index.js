18fcf9e7352e18585c61eddbd96cbae67d84f5ee
MydarMo <hameedahmohammed7@yahoo.com>
2024-01-22 17:08:06 +0000

    fix tranverse errors in the api index file

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import { request } from "./helpers";
+import { request } from './helpers';
 
 /**
  * Pull vehicles information
@@ -8,34 +8,39 @@ import { request } from "./helpers";
  */
 // TODO: All API related logic should be made inside this function.
 export default async function getData() {
-    // fetch the list of vehicles
-    const response = await fetch("/api/vehicles.json");
-    if (!response.ok) {
-        // Handle HTTP error
-        throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
+  // fetch the list of vehicles
+  const response = await fetch('/api/vehicles.json');
+  if (!response.ok) {
+    // Handle HTTP error
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+  const data = await response.json();
 
-    // map through the list of vehicles to access the apiUrl for each vehicle to return the list of only successful APIs
-    const vehicleData = await Promise.allSettled(
-        data.map(async (res) => (await fetch(res.apiUrl)).json())
+  // map through the list of vehicles to access the apiUrl to return json
+  const resolvedData = await Promise.allSettled(
+    data.map(async (res) => (await fetch(res.apiUrl)).json())
+  );
+
+  // filter through the list of apiUrl data to return all sucessfully fulfilled data
+  const fulfilledData = resolvedData.filter(
+    (resolve) => resolve.status !== 'rejected'
+  );
+
+  // filter through the fulfilled list to remove vehicles without price
+  const vehiclesWithPrice = fulfilledData.filter(
+    (fulfilled) => fulfilled.value.price
+  );
+
+  // merge the initial data and the vehicles with price to return complete object
+  const vehicles = vehiclesWithPrice.map((vehicle) => {
+    const initialData = data.find(
+      (initial) => initial.id === vehicle.value.id
     );
+    return {
+      ...initialData,
+      ...vehicle.value,
+    };
+  });
 
-    console.log({ vehicleData, data });
-
-    // merge both data and vehicleData to form a complete json object for each car, excluding unfilfilled response and vehicles without price
-    const vehicles = [];
-
-    for (let i = 0; i < data.length; i++) {
-        if (
-            vehicleData[i].status === "fulfilled" &&
-            vehicleData[i].value.price
-        ) {
-            vehicles.push({ ...data[i], ...vehicleData[i].value });
-        } else {
-            console.log(`HTTP error! Status: failed`);
-        }
-    }
-
-    return vehicles;
+  return vehicles;
 }
